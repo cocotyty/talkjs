71693887cc43749361d7f9e7ecd077a224262706
cocotyty <cocotyty@sina.com>
2017-01-26 11:45:57 +0800

    update klass

--- klass.js
+++ klass.js
@@ -9,22 +9,22 @@
             name = null;
             parent = Object;
         }
-        function cls() {
+        function Type() {
             var constructor = this['constructor'];
             return constructor && constructor.apply(this, arguments);
         }
         if (name) {
-            global[name] = cls;
+            global[name] = Type;
         }
-        function Type() { }
-        Type.prototype = parent.prototype;
-        cls.prototype = new Type();
+        function Temp() { }
+        Temp.prototype = parent.prototype;
+        Type.prototype = new Temp();
         for (var i in proto) {
             if (proto.hasOwnProperty(i) && typeof proto[i] == 'function') {
-                cls.prototype[i] = proto[i];
+                Type.prototype[i] = proto[i];
             }
         }
-        return cls;
+        return Type;
     }
     global.Klass = klass;
 })(this)
